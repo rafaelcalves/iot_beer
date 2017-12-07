6970965069e08267dc0446562c8128002bc79234
Rafael CorRêa Alves <rafael.correa@sap.com>
2017-12-07 15:41:53 -0200

    Created springboot project

---
+++ project1/high-level/src/main/java/com/sap/ibso/iot_beer/IotBeerApplication.java
@@ -0,0 +1,12 @@
+package com.sap.ibso.iot_beer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotBeerApplication.class, args);
+	}
+}
