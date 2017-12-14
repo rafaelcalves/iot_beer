665cf71a06ca945d7f97cd4518e8fac82f8ede11
i863409 <luciano.freitas@sap.com>
2017-12-14 12:31:40 -0200

    Correcting led blinking logic, properly initializing pins and removing logging

--- project2/low-level/src/main/java/com/sap/iotbeerlowlevel/Main.java
+++ project2/low-level/src/main/java/com/sap/iotbeerlowlevel/Main.java
@@ -10,30 +10,29 @@ import com.pi4j.io.gpio.PinMode;
 import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import java.util.logging.Logger;
 
 public class Main {
-	final static Logger log = Logger.getLogger(Main.class);
+	final static Logger log = Logger.getLogger(Main.class.toString());
+
+	static GpioController gpio;
+
+	// provision gpio pins #04 as an output pin and make sure is is set to LOW at startup
+	static GpioPinDigitalOutput myLed;
 
 	public static void main(String[] args){
+		initializePins();
 
-		//testBlink();
-
-		// create gpio controller
-		final GpioController gpio = GpioFactory.getInstance();
-
-		// define gpio pin number 1 as an output pin and turn it off
-		final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(
-				RaspiPin.GPIO_04, "LED", PinState.LOW);
+		testBlink();
 
 		// set shutdown state for pin 1 (LED)
-		pin.setShutdownOptions(true, PinState.LOW);
+		//myLed.setShutdownOptions(true, PinState.LOW);
 
+		/*
 		try {
 			// toggle pin state for 25 times
 			for (int i = 0; i < 25; i++) {
-				pin.toggle();
+				myLed.toggle();
 				log.log(Level.INFO, pin.getState());
 				Thread.sleep(2500);
 			}
@@ -42,7 +41,19 @@ public class Main {
 		} catch (InterruptedException e) {
 			log.log(Level.ERROR, e);
 		}
+		*/
 	}
+
+	private static void initializePins() {
+		// create gpio controller
+		final GpioController gpio = GpioFactory.getInstance();
+
+		// define gpio pin number 1 as an output pin and turn it off
+		final GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04,   // PIN NUMBER
+																		  "MyLED",           // PIN FRIENDLY NAME (optional)
+																		  PinState.LOW);      // PIN STARTUP STATE (optional)
+	}
+
 	private static void testBlink(){
 		for(int i=0; i<30; i++) {
 			toggleLed();
@@ -50,35 +61,16 @@ public class Main {
 	}
 
 	private static void toggleLed(){
-		// create gpio controller instance
-		final GpioController gpio = GpioFactory.getInstance();
-
-		// provision gpio pins #04 as an output pin and make sure is is set to LOW at startup
-		GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04,   // PIN NUMBER
-																	"My LED",           // PIN FRIENDLY NAME (optional)
-																	PinState.LOW);      // PIN STARTUP STATE (optional)
-
-		// explicitly set a state on the pin object
-		myLed.setState(PinState.HIGH);
-
-		// use convenience wrapper method to set state on the pin object
-		myLed.low();
-		myLed.high();
-
-		// use toggle method to apply inverse state on the pin object
-		myLed.toggle();
-
 		// use pulse method to set the pin to the HIGH state for
 		// an explicit length of time in milliseconds
 		myLed.pulse(1000);
 
+		myLed.low(); // OR myLed.setState(PinState.HIGH); OR
 
 		gpio.unprovisionPin(myLed);
 	}
 
 	private static void readPinAndWriteOutput(){
-		final GpioController gpio = GpioFactory.getInstance();
-
 		// provision gpio pin #02 as an input pin with its internal pull down resistor enabled
 		// (configure pin edge to both rising and falling to get notified for HIGH and LOW state
 		// changes)
@@ -95,4 +87,3 @@ public class Main {
 		gpio.unprovisionPin(myButton);
 	}
 }
-
